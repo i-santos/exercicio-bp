222985f2008d97c9bee68ba87bc87380c55be6a6
i. dos santos <iosorio.santos@gmail.com>
2021-11-05 12:58:01 -0300

    add implementation to check content accessibility by role

--- src/takehome/core.clj
+++ src/takehome/core.clj
@@ -1,9 +1,27 @@
-(ns takehome.core
-  (:require [java-time :as time]))
+(defn can-usuario-access? [content]
+  (case (:type content)
+    :series true
+    false))
 
-(defn can-access? [object purchase]
-  (if (= (:type object) :movie)
-    (and (= (:type purchase) :patriota)
-         (time/before? (:subscription-start purchase)
-                       (:released-at object)
-                       (:subscription-end purchase)))))
+(defn can-patriota-access? [content]
+  (or (can-usuario-access? content)
+      (case (:type content)
+        :podcast true
+        :debate true
+        :interview true
+        false)))
+
+
+(defn can-premium-access? [content]
+  (or (can-patriota-access? content)
+      (case (:type content)
+        :course true
+        false)))
+
+(defn can-mecenas-access? [content]
+  (or (can-premium-access? content)
+      (case (:type content)
+        :patron true
+        false)))
+
+
